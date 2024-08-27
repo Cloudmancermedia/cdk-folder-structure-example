f65d6209549b529696392ddda1c2f434cf18ea21
Ryan Williams <52295078+Cloudmancermedia@users.noreply.github.com>
2024-08-27 11:31:29 -0600

    repo tweaks, working now

--- stacks/cdk-folder-structure-example-stack.ts
+++ stacks/cdk-folder-structure-example-stack.ts
@@ -1,21 +1,33 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
+import { Function, Runtime, Code, LayerVersion } from 'aws-cdk-lib/aws-lambda';
 import { RestApi, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
-import { join } from 'path';
 
 export class CdkFolderStructureExampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const layer = new LayerVersion(
+      this,
+      "Layer",
+      {
+        code: Code.fromAsset("src/layers"),
+        compatibleRuntimes: [ Runtime.NODEJS_LATEST ],
+        layerVersionName: "NodeJsLayer"
+      }
+    )
+
     // Define the Lambda function
     const processDataFunction = new Function(this, 'ProcessDataFunction', {
-      runtime: Runtime.NODEJS_18_X,
-      handler: 'process-data.handler',
-      code: Code.fromAsset(join(__dirname, '../dist/src/lambdas')),
+      runtime: Runtime.NODEJS_LATEST,
+      handler: 'lambdas/process-data.handler',
+      code: Code.fromAsset('dist/src'),
       environment: {
         LOG_LEVEL: 'INFO',
       },
+      layers: [
+        layer
+      ],
     });
 
     // Define the API Gateway
@@ -26,6 +38,6 @@ export class CdkFolderStructureExampleStack extends Stack {
 
     // Create an API Gateway resource and method for the Lambda function
     const processDataIntegration = new LambdaIntegration(processDataFunction);
-    api.root.addMethod('POST', processDataIntegration);
+    api.root.addResource('data').addMethod('POST', processDataIntegration);
   }
 }
